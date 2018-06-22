af8ce6f76a909c12f56b70c90629d153985c809b
km45 <km45@users.noreply.github.com>
2018-06-22 18:14:38 +0900

    Add scoop.ps1

---
+++ scoop.ps1
@@ -0,0 +1,42 @@
+function LOGD($message) {
+    Write-Output("[debug] $message")
+}
+function LOGI($message) {
+    Write-Output("[info] $message")
+}
+
+# -----------------------------------------------------------------------------
+# Install / Update scoop
+# -----------------------------------------------------------------------------
+if (Get-Command scoop -ErrorAction SilentlyContinue) {
+    LOGI("Scoop is already installed, so try updating.")
+    scoop update
+}
+else {
+    # Run below command if needed:
+    # `set-executionpolicy remotesigned -s currentuser`.
+    LOGI("Scoop is not installed, so install it.")
+    Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+# -----------------------------------------------------------------------------
+# Install packages
+# -----------------------------------------------------------------------------
+$packages = @(
+    # 'git',  # Cannot use Windows Explorer integration "Git Bash Here".
+    'jq'
+)
+foreach ($package in $packages) {
+    LOGD("Check whether $package is already installed or not.")
+    $installed = (scoop list 6>&1 | Select-String -SimpleMatch -Quiet $package)
+    LOGD("--> $installed")
+
+    if ($installed) {
+        echo "Update $package"
+        scoop update $package
+    }
+    else {
+        echo "Install $package"
+        scoop install $package
+    }
+}
