86537d9bd487b9f629069a833d0e686a935ec65f
Kamei Shogo <km45@users.noreply.github.com>
2024-09-04 22:29:55 +0900

    Use Next.js Server Actions

--- nextjs-trial/app/page.tsx
+++ nextjs-trial/app/page.tsx
@@ -1,18 +1,28 @@
-import { Canvas } from "../components/Canvas";
+"use client"
+
+import { Canvas, Edge, Node } from "../components/Canvas";
+import { generateGraphOnServer } from "./server";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [nodes, setNodes] = useState<Node[]>([]);
+  const [edges, setEdges] = useState<Edge[]>([]);
+
+  async function formToGraph(formData: FormData) {
+    const res = await generateGraphOnServer(formData);
+
+    setNodes(res.nodes);
+    setEdges(res.edges);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Canvas
-        nodes={[
-          { id: "1", label: "Node 1", x: 0, y: 0, size: 10, color: "blue" },
-          { id: "2", label: "Node 2", x: 1, y: 1, size: 20, color: "red" },
-          { id: "3", label: "Node 3", x: 1, y: 2, size: 30, color: "green" },
-        ]}
-        edges={[
-          { start_node_id: "1", end_node_id: "2", size: 5, color: "purple" },
-        ]} />
+      <form action={formToGraph}>
+        <input name="a" />
+        <button>form to graph</button>
+      </form>
+      <Canvas nodes={nodes} edges={edges} />
     </main>
   );
 }
