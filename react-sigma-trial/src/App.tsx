b1194b521bb74e4eb773b0b93afb252a43e9ae37
Kamei Shogo <km45@users.noreply.github.com>
2025-01-24 00:34:13 +0900

    Layout

--- react-sigma-trial/src/App.tsx
+++ react-sigma-trial/src/App.tsx
@@ -11,8 +11,6 @@ import "@react-sigma/core/lib/style.css";
 import { MultiDirectedGraph } from "graphology";
 import { Edge, Node, updatePrompts } from "./server";
 
-const sigmaStyle = { height: "500px", width: "500px" };
-
 function MyGraph(props: { nodes?: Node[]; edges?: Edge[] }) {
   const loadGraph = useLoadGraph();
 
@@ -41,34 +39,63 @@ function App() {
   const [state, submitAction] = useActionState(updatePrompts, {});
 
   return (
-    <>
-      <div>
-        <>
-          <div>
-            <form action={submitAction}>
-              <input type="hidden" name="action" value="add" />
-              <input type="text" name="prompt" />
-            </form>
+    <div className="h-dvh flex flex-col">
+      <div className="navbar bg-red-400">
+        <div className="">
+          <div className="dropdown dropdown-hover">
+            <summary className="btn">
+              <div className="indicator">
+                <div className="indicator-item">{state.prompts?.length}</div>
+                prompts
+              </div>
+            </summary>
+            <ul className="dropdown-content menu bg-base-100">
+              {state.prompts?.map((prompt, index) => (
+                <li key={index}>
+                  <form action={submitAction}>
+                    <input type="hidden" name="action" value="remove" />
+                    <input type="hidden" name="index" value={index} />
+                    <input type="submit" value={prompt} />
+                  </form>
+                </li>
+              ))}
+            </ul>
           </div>
-        </>
-        {state.prompts?.map((prompt, index) => (
-          <div key={index}>
-            <form action={submitAction}>
-              <input type="hidden" name="action" value="remove" />
-              <input type="hidden" name="index" value={index} />
-              <input type="submit" value={prompt} />
-            </form>
-          </div>
-        ))}
+        </div>
+        <div className="">
+          <form action={submitAction} className="flex">
+            <div
+              className="tooltip tooltip-bottom flex-none"
+              data-tip="short input interpretation"
+            >
+              <select defaultValue="dummy" className="select">
+                <option>dummy</option>
+                <option disabled={true}>trail</option>
+                <option disabled={true}>node</option>
+              </select>
+            </div>
+            <input type="hidden" name="action" value="add" />
+            <input
+              type="text"
+              className="input grow"
+              name="prompt"
+              placeholder="type a prompt here"
+            />
+          </form>
+        </div>
+        <div className="">sample</div>
       </div>
-      <SigmaContainer style={sigmaStyle}>
-        <MyGraph nodes={state.nodes} edges={state.edges} />
-        <ControlsContainer position={"bottom-right"}>
-          <ZoomControl />
-          <FullScreenControl />
-        </ControlsContainer>
-      </SigmaContainer>
-    </>
+
+      <div className="flex-grow">
+        <SigmaContainer>
+          <MyGraph nodes={state.nodes} edges={state.edges} />
+          <ControlsContainer position={"bottom-right"}>
+            <ZoomControl />
+            <FullScreenControl />
+          </ControlsContainer>
+        </SigmaContainer>
+      </div>
+    </div>
   );
 }
 
